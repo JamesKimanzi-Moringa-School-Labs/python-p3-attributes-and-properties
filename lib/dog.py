a6c0cd2d7eb57bfe17f05b466d2f543b5c6e2943
James-Kimanzi <jameskimanzi@gmail.com>
2024-05-07 02:35:25 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,39 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+
+    def __init__(self, name="", breed=""):
+        self._name = ""
+        self._breed = ""
+        self.set_name(name)
+        self.set_breed(breed)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if type(value) == str and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in self.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+    
+
+max = Dog('puppy', 'Corgi')
+
+print(max.name)
+
+print (max.breed)
