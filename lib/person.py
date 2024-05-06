5479c6605f91e36690b4b81ba4773a408b7ebf62
James-Kimanzi <jameskimanzi@gmail.com>
2024-05-07 02:52:16 +0300

    completed assignment

--- lib/person.py
+++ lib/person.py
@@ -17,7 +17,8 @@ APPROVED_JOBS = [
 
 class Person:
     approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
-    def __init__(self, name ="", job =""):
+
+    def __init__(self, name="", job=""):
         self._name = ""
         self._job = ""
         self.name = name
@@ -25,22 +26,23 @@ class Person:
 
     def get_name(self):
         return self._name
-    def set_name(self, name):
-        if type(name) == str and 1 <= len(name) <= 25:
-            self._name = name.title()
 
+    def set_name(self, value):
+        if type(value) == str and 1 <= len(value) <= 25:
+            self._name = value.title()
         else:
-            print('Name must be string between 1 and 25 characters.')
+            print("Name must be a string between 1 and 25 characters.")
 
     name = property(get_name, set_name)
 
     def get_job(self):
         return self._job
-    def set_job(self, job):
-        if job in self.approved_jobs:
-            self._job = job
+
+    def set_job(self, value):
+        if value in self.approved_jobs:
+            self._job = value
         else:
-            print('Job must be in list of approved jobs.')
+            print("Job must be in list of approved jobs.")
 
     job = property(get_job, set_job)
 
